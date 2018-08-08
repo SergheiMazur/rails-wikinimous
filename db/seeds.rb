9018ea84a44c8307358b147a5fc0b89715cc1243
SergheMazur <serhio1007@gmail.com>
2018-08-07 23:40:42 -0400

    add delete button

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  task = Article.new(title: Faker::Dessert.topping, content: Faker::Robin.quote)
+  task.save
+end
